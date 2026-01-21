9be23b731dd94cb94c8e6e397d1adf79112952c3
Saadbinzahid1 <sbinzahid2@gmail.com>
2026-01-21 16:18:44 +0500

    Added the smooth scroll functionality on Learn More button

--- script.js
+++ script.js
@@ -15,6 +15,13 @@ const openModal = function (e) {
 };
 btnsOpenModal.forEach((btn) => btn.addEventListener("click", openModal));
 
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
+btnScrollTo.addEventListener("click", function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  section1.scrollIntoView({ behavior: "smooth" });
+});
+
 const closeModal = function () {
   modal.classList.add("hidden");
   overlay.classList.add("hidden");
